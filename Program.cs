cbb0da141c178242a4b522a946ec0b780ec7c72f
BhagyashriSabale <balisabale16@gmail.com>
2023-05-26 09:10:46 +0530

    [Bhagyashri] Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LambdaExpressionUserProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Lambda Expression Problem");
+        }
+    }
+}
