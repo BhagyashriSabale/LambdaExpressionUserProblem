832293b7ea454b08fc4e92b7260aee7f5bd86e0a
BhagyashriSabale <balisabale16@gmail.com>
2023-05-26 09:18:05 +0530

    [Bhagyashri] Enter valid First name

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LambdaExpressionUserProblem
+﻿using System.Text.RegularExpressions;
+
+namespace LambdaExpressionUserProblem
 {
     internal class Program
     {
+        public static bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, @"^[A-Z][a-zA-Z]{2,}$");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Lambda Expression Problem");
+            Console.Write("Enter a valid First Name: ");
+            string firstName = Console.ReadLine();
+            bool isFirstNameValid = ValidateFirstName(firstName);
+            Console.WriteLine("First Name is " + (isFirstNameValid ? "valid" : "invalid"));
         }
     }
 }
